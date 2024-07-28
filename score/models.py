f7072869652e66f9c89be63cf541def1de29520b
Mateus Alves <mateusalvesj3050@gmail.com>
2024-07-28 20:02:09 -0300

    Inserting meta data on the website model

--- score/models.py
+++ score/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Score(models.Model):
+    class Meta:
+        ordering = ['-value']
+
     name = models.CharField(max_length=50)
     value = models.PositiveSmallIntegerField()
 
