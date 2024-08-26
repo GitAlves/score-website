67c8188cec9879b0f50f72fc7e657e937ca6a7a6
Mateus Alves <mateusalvesj3050@gmail.com>
2024-08-26 14:26:24 -0300

    Adding form informations in the index function

--- score/views.py
+++ score/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from score.models import Score
+from score.forms import ScoreForm
 
 
 def index(request):
     context = {}
+    form = ScoreForm()
     scores = Score.objects.all()
     context['scores'] = scores
     context['title'] = 'Home'
+    context['form'] = form
     return render(request, 'index.html', context)
 
 
